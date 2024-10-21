bea04286f68478ce1a03511ed196c432631eb2b1
Pistcov <ilyapistcov@yandex.ru>
2024-10-21 23:58:00 +0300

    dev

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function toggleButton() {
-    const button = document.getElementById('toggleButton');
-    const background = document.getElementById('background');
+$(document).ready(function() {
+    $("#hi").click(function() {
+        $(this).addClass("hidden");
+        $(".layer").addClass("clicked");
+    });
 
-    // Меняем состояние кнопки на неактивное
-    button.disabled = true;
-
-    // Меняем фон на "занято"
-    background.classList.remove('free');
-    background.classList.add('busy');
-}
+    $("#remove").click(function() {
+        $(".layer").removeClass("clicked");
+        $("#hi").removeClass("hidden");
+    });
+});
